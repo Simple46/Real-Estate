32dfbf95ed181889e9ca3e479a7a3685409940ec
imple46 <olayiwolahissa484@gmail.com>
2025-08-23 21:09:23 -0700

    Real estate website upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth flexCenter c-container">
+                {/* left side */}
+                <div className="c-left">
+                 left
+                </div>
+ 
+                {/* right side  */}
+                <div className="c-right">
+                    <div className="img-container">
+                        <img src="./contact.jpg" alt=""/>
+                </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact
+
